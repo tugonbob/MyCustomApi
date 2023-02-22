36ba8a2e9073ab30724bb25959b8489b3d3ed4a3
Joshua Gao <joshuakgao@gmail.com>
2023-02-22 15:19:39 -0600

    Api created

---
+++ MyApi.py
@@ -0,0 +1,24 @@
+from flask import *
+import json
+import time
+
+app = Flask(__name__)
+
+@app.route('/demo', methods=['GET'])
+def demo():
+    data = {'page': 'Demo', 'message': 'Successfully called api demo', 'timestamp': time.time()}
+    json_dump = json.dumps(data)
+
+    return json_dump
+
+
+@app.route('/user/', methods=['GET'])
+def user():
+    user_query = str(request.args.get('user'))
+    data = {'page': 'User Info', 'message': f'{user_query}', 'timestamp': time.time()}
+    json_dump = json.dumps(data)
+
+    return json_dump
+
+if __name__ == '__main__':
+     app.run(port=8888)
