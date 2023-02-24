9496563aa4888b170f8e13acdedeb51d5de76391
Joshua Gao <joshuakgao@gmail.com>
2023-02-24 17:35:02 -0600

    Post request written

--- ApiCaller.py
+++ ApiCaller.py
@@ -1,7 +1,16 @@
 import requests
+import json
 
-with open('NgrokServerUrl.txt', 'r') as f:
-    url = f.read()
 
-response = requests.get(f"{url}")
-print(response.json())
+def post(query):
+    url = 'http://127.0.0.1:8888'
+    payload = {'query': query}
+    response = requests.post(url=url, data=payload)
+    return response.json()
+
+def get(query):
+    url = f'http://127.0.0.1:8888/?query={query}'
+    response = requests.get(url)
+    return response.json()
+
+print(post('hi'))
--- MyApi.py
+++ MyApi.py
@@ -1,24 +1,19 @@
-from flask import *
+from flask import Flask, request
 import json
-import time
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def demo():
-    data = {'page': 'Demo', 'message': 'Successfully called api demo', 'timestamp': time.time()}
-    json_dump = json.dumps(data)
+    if request.method == 'GET':
+        query = str(request.args.get('query'))
+        data = {'method': "GET", 'query': query}
+        return json.dumps(data)
+    else:
+        query = request.form['query']
+        data = {'method': 'POST', 'query': query}
+        return json.dumps(data)
 
-    return json_dump
-
-
-@app.route('/user/', methods=['GET'])
-def user():
-    user_query = str(request.args.get('user'))
-    data = {'page': 'User Info', 'message': f'{user_query}', 'timestamp': time.time()}
-    json_dump = json.dumps(data)
-
-    return json_dump
 
 if __name__ == '__main__':
      app.run(port=8888)
